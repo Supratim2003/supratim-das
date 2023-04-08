b0db149840b6711c4a61d138c8a294cd3eaadc8b
Supratim2003 <113002763+Supratim2003@users.noreply.github.com>
2023-04-08 23:24:38 +0530

    piggi_bank

---
+++ Pythonp/piggi_bank.py
@@ -0,0 +1,6 @@
+a=int(input("enter the number of 10 rupes coin:"))
+b=int(input("enter the number of 5 rupes coin:"))
+c=int(input("enter the number of 2 rupes coine:"))
+d=int(input("enter the number of 1 rupes coine:"))
+total_money= (10*a)+(5*b)+(2*c)+(1*d)
+print("Total money in piggi bank: ",total_money,"rupee")
